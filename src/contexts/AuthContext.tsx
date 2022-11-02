2e08e384898d4128668a10bed7c8cd4ca84f8c60
Davi Marcilio <davimarcilio.js@gmail.com>
2022-11-02 12:28:36 -0300

    google authenticator

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,4 +1,8 @@
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useState, useEffect } from "react";
+import * as Google from 'expo-auth-session/providers/google';
+import * as AuthSession from 'expo-auth-session';
+import * as WebBrowser from 'expo-web-browser';
+WebBrowser.maybeCompleteAuthSession();
 interface UserProps {
     name: String,
     avatarUrl: String,
@@ -8,23 +12,47 @@ interface AuthProviderProps {
 }
 export interface AuthContextDataProps {
     user: UserProps,
+    isUserLoading: boolean,
     signIn: () => Promise<void>;
 
 }
 export const AuthContext = createContext({} as AuthContextDataProps);
 
 export function AuthContextProvider({ children }: AuthProviderProps) {
-    async function signIn() {
-        console.log('Vamos Logar!');
+    const [user, setUser] = useState<UserProps>({} as UserProps)
+    const [isUserLoading, setIsUserLoading] = useState(false);
+    const [req, res, promptAsync] = Google.useAuthRequest({
+        clientId: '841451846318-v7tkbakjcvj5nke1nbu5416lqnu07hlt.apps.googleusercontent.com',
+        redirectUri: AuthSession.makeRedirectUri({ useProxy: true, }),
+        scopes: ['profile', 'email']
+    })
+    async function signInWithGoogle(access_token: string) {
+        console.log('TOKEN DE AUTENTICAÇÃO =>', access_token);
+
 
     }
+    async function signIn() {
+        try {
+            setIsUserLoading(true)
+            await promptAsync();
+        } catch (error) {
+            console.log(error);
+            throw error;
+        } finally {
+            setIsUserLoading(false);
+        }
+
+    }
+    useEffect(() => {
+        if (res?.type === 'success' && res.authentication?.accessToken) {
+            signInWithGoogle(res.authentication.accessToken);
+        }
+    }, [res])
     return (
         <AuthContext.Provider value={{
             signIn,
-            user: {
-                name: 'Henrique',
-                avatarUrl: 'https://github.com/RickFernandess.png'
-            }
+            isUserLoading,
+            user,
         }}>
             {children}
         </AuthContext.Provider>
--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,11 +1,12 @@
 import { Center, Text, Icon } from 'native-base';
+
 import { Fontisto } from '@expo/vector-icons';
 import Logo from '../assets/logo.svg';
 import { Button } from '../components/Button';
 import { useAuth } from '../hooks/useAuth';
 export function SignIn() {
   const { signIn, user } = useAuth();
-  console.log('DADOS DO USUÁRIO =>', user)
+
   return (
     <Center flex={1} bgColor="gray.900" p={7}>
       <Logo width={212} height={40} />
