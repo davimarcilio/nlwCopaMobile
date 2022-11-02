5e6b97c1663a3ffd96c7bdb8be5e1e875065ba4c
Davi Marcilio <davimarcilio.js@gmail.com>
2022-11-02 13:04:02 -0300

    end find interface

--- src/screens/Find.tsx
+++ src/screens/Find.tsx
@@ -8,13 +8,11 @@ import { Button } from "../components/Button";
 export function Find() {
     return (
         <VStack flex={1} bgColor={'gray.900'}>
-            <Header title="Criar novo bolão" />
+            <Header title="Criar novo bolão" showBackButton />
             <VStack mt={8} mx={5} alignItems="center">
-                <Logo />
-                <Heading fontFamily={'heading'} color={'white'} fontSize={'xl'} my={8} textAlign={'center'}>Encontre um bolão através de {'\n'} seu código único</Heading>
+                <Heading fontFamily={'heading'} color={'white'} fontSize={'xl'} mb={8} textAlign={'center'}>Encontre um bolão através de {'\n'} seu código único</Heading>
                 <Input mb={2} placeholder={'Qual nome do seu bolão?'} />
                 <Button title={'BUSCAR BOLÃO'} />
-                <Text color={'gray.200'} fontSize={'sm'} textAlign={'center'} px={10} mt={4}>Após criar seu bolão, você receberá um código único que poderá usar para convidar outras pessoas.</Text>
             </VStack>
         </VStack>
     )
