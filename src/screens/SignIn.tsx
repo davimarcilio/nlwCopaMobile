52686d95c0845ff0dcc6732703b39d1efae47452
Davi Marcilio <davimarcilio.js@gmail.com>
2022-11-02 01:57:50 -0300

    theme unitilized

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,12 +1,11 @@
 import { Center, Text } from 'native-base'
-import { THEME } from "../styles/theme";
 
-export function SignIn(){
-  return(
-      <Center flex={1} bgColor="gray.900">
-        <Text color="white" fontSize={20}>
-          Sign In
-        </Text>
-      </Center>
+export function SignIn() {
+  return (
+    <Center flex={1} bgColor="gray.900">
+      <Text color="white" fontSize={20}>
+        Sign In
+      </Text>
+    </Center>
   )
 }
