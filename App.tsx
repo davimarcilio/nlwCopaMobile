402e0dc0e6a2053c3ff9a9398dff0f55bcbd50f1
Davi Marcilio <davimarcilio.js@gmail.com>
2022-11-03 21:54:15 -0300

    start navegators

--- App.tsx
+++ App.tsx
@@ -30,7 +30,7 @@ export default function App() {
           backgroundColor="transparent"
           translucent
         />
-        {fontsLoaded ? <Pools /> : <Loading />}
+        {fontsLoaded ? <SignIn /> : <Loading />}
       </AuthContextProvider>
     </NativeBaseProvider>
   )
