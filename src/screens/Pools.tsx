3f9d1fc5f590ac6b332e9f27d45db0fa12c67b91
Davi Marcilio <davimarcilio.js@gmail.com>
2022-11-05 10:11:36 -0300

    loading and empty list added

--- src/screens/Pools.tsx
+++ src/screens/Pools.tsx
@@ -1,17 +1,57 @@
-import { VStack, Icon } from "native-base";
+import { VStack, Icon, useToast, FlatList } from "native-base";
+import { useEffect, useState } from "react";
 import { Button } from "../components/Button";
 import { Header } from "../components/Header";
 import { Octicons } from '@expo/vector-icons';
-import { useNavigation } from '@react-navigation/native'
+import { useNavigation } from '@react-navigation/native';
+import { PoolCard, PoolProps } from "../components/PoolCard";
+import { api } from "../services/api";
+import { Loading } from "../components/Loading";
+import { EmptyPoolList } from "../components/EmptyPoolList";
+
+import { ItemClick } from "native-base/lib/typescript/components/composites/Typeahead/useTypeahead/types";
 
 export function Pools() {
+    const [isLoading, setIsLoading] = useState(true);
+    const [pools, setPools] = useState<PoolProps[]>([]);
+    const toast = useToast();
     const navigation = useNavigation();
+    async function fetchPools() {
+        try {
+            setIsLoading(true);
+            const response = await api.get('/pools')
+            setPools(response.data.pools)
+
+        } catch (error) {
+            console.log(error);
+            toast.show({
+                title: 'Não foi possível carregar os bolões',
+                placement: 'top',
+                bgColor: 'red.500'
+            })
+        } finally {
+            setIsLoading(false)
+        }
+    }
+    useEffect(() => {
+        fetchPools();
+    }, [])
     return (
         <VStack flex={1} bgColor={'gray.900'}>
             <Header title={'Meus bolões'} />
             <VStack mt={6} mx={5} borderBottomWidth={1} borderBottomColor={'gray.600'} pb={4} mb={4}>
                 <Button leftIcon={<Icon as={Octicons} name={'search'} color={'black'} size={'md'} />} title={'BUSCAR BOLÃO POR CÓDIGO'} onPress={() => navigation.navigate('find')} />
             </VStack>
+            {isLoading ? <Loading /> :
+                <FlatList
+                    data={pools}
+                    keyExtractor={item => item.id}
+                    renderItem={({ item }) => <PoolCard data={item} />}
+                    px={5}
+                    showsVerticalScrollIndicator={false}
+                    _contentContainerStyle={{ pb: 10 }}
+                    ListEmptyComponent={() => <EmptyPoolList />}
+                />}
         </VStack>
     )
 }
